2e93ba98af51a09b35d7a558bae341bb1a767c04
Jownsu <jhonesdigno666@gmail.com>
2024-06-08 16:16:13 +0800

    mobile of link page

--- app/(pages)/links/_components/LinkList.tsx
+++ app/(pages)/links/_components/LinkList.tsx
@@ -9,20 +9,23 @@ const LinkList = () => {
 
     return (
         <div>
-            <Button 
-                variant={"outline"} 
-                size={"full"} 
+            <Button
+                variant={"outline"}
+                size={"full"}
                 type="submit"
+                className="mb-[2.4rem]"
             >
                 + Add new link
             </Button>
             {!!links.length && <p>Render List</p>}
 
             {!links.length && (
-                <div>
-                    <EmptyIcon />
-                    <h2>Let&apos;s get you started</h2>
-                    <p>
+                <div className="mb-[2.4rem] rounded-[1.2rem] bg-white px-[2rem] py-[4.6rem] text-center">
+                    <EmptyIcon className="mx-auto mb-[2.4rem] h-[8rem] w-[12.477rem]" />
+                    <h2 className="mb-[2.2rem] text-[2.4rem] font-bold text-dark_grey">
+                        Let&apos;s get you started
+                    </h2>
+                    <p className="text-dark_grey">
                         Use the &quot;Add new link&quot; button to get started.
                         Once you have more than one link, you can reorder and
                         edit them. We&apos;re here to help you share your
@@ -31,7 +34,10 @@ const LinkList = () => {
                 </div>
             )}
 
-            <Button size="full">Save</Button>
+            <div className="mb-[2.4rem] h-[.1rem] w-[calc(100%_+_4.8rem)] translate-x-[-2.4rem] bg-borders"></div>
+            <Button size="full" disabled>
+                Save
+            </Button>
         </div>
     );
 };
--- app/(pages)/links/page.tsx
+++ app/(pages)/links/page.tsx
@@ -2,13 +2,17 @@ import LinkList from "./_components/LinkList";
 
 const LinkPage = () => {
     return (
-        <div>
-            <h1>Customize your links</h1>
-            <p>
-                Add/edit/remove links below and then share all your profiles
-                with the world!
-            </p>
-            <LinkList />
+        <div className="container py-[2.4rem]">
+            <div className="rounded-[1.2rem] p-[2.4rem]">
+                <h1 className="text-[2.4rem] font-bold leading-[150%] text-dark_grey">
+                    Customize your links
+                </h1>
+                <p className="mb-[4.1rem] text-grey">
+                    Add/edit/remove links below and then share all your profiles
+                    with the world!
+                </p>
+                <LinkList />
+            </div>
         </div>
     );
 };
