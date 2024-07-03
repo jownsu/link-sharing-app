8603d630c1ad3e6e9010cf51022cd7ff8a8928a9
Jownsu <jhonesdigno666@gmail.com>
2024-07-03 22:19:01 +0800

    Setup react hook form for devlinks operations

--- app/(pages)/links/_components/DevLinkItem.tsx
+++ app/(pages)/links/_components/DevLinkItem.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { DevLink } from "@/app/_constants/constants";
-import LinkIcon from "@/public/icons/icon-link.svg";
-import DragIcon from "@/public/icons/icon-drag-and-drop.svg";
-import classNames from "classnames";
-import { Platforms } from "@/app/_constants/constants";
-import useDevLinkStore from "@/app/_store/devlink.store";
 import {
     Select,
     SelectContent,
@@ -13,23 +7,30 @@ import {
     SelectTrigger,
     SelectValue
 } from "@/app/_components/Select";
+import { DevLink, Platforms } from "@/app/_constants/constants";
+import DragIcon from "@/public/icons/icon-drag-and-drop.svg";
+import LinkIcon from "@/public/icons/icon-link.svg";
+import classNames from "classnames";
+import { Controller, useFormContext } from "react-hook-form";
 import PlatformIcon from "./PlatformIcon";
 
 interface Props {
     link: DevLink;
     index: number;
+    remove: (id: number) => void;
 }
 
-const DevLinkItem = ({ link, index }: Props) => {
-
-    const removeDevLink = useDevLinkStore(state => state.removeDevLink);
+const DevLinkItem = ({ link, index, remove }: Props) => {
+    const { register, control } = useFormContext();
 
     return (
         <li key={link.id} className="rounded-[1.2rem] bg-light_grey p-[2rem]">
             <div className="mb-[1rem] flex items-center gap-[1rem] text-grey">
                 <DragIcon />{" "}
                 <span className="mr-auto font-bold">Link #{index + 1}</span>
-                <button type="button" onClick={() => removeDevLink(link.id)}>Remove</button>
+                <button type="button" onClick={() => remove(index)}>
+                    Remove
+                </button>
             </div>
 
             <div className="mb-[.5rem]">
@@ -39,25 +40,37 @@ const DevLinkItem = ({ link, index }: Props) => {
                 >
                     Platform
                 </label>
-                <Select defaultValue="Github">
-                    <SelectTrigger className="h-[4.8rem] rounded-[.8rem] border border-borders pl-[1rem] pr-[1.5rem] text-[1.6rem] text-grey aria-expanded:border-primary aria-expanded:shadow-drop_primary">
-                        <SelectValue />
-                    </SelectTrigger>
-                    <SelectContent className="z-50 bg-white px-[1.6rem]">
-                        {Object.values(Platforms).map((platform) => (
-                            <SelectItem
-                                key={platform.id}
-                                value={platform.label}
-                                className="border-b-borders px-0 py-[1.2rem] text-[1.6rem] text-dark_grey [&:not(:last-of-type)]:border-b"
-                            >
-                                <div className="flex items-center gap-[1rem]">
-                                    <PlatformIcon platform={platform.icon} />
-                                    {platform.label}
-                                </div>
-                            </SelectItem>
-                        ))}
-                    </SelectContent>
-                </Select>
+
+                <Controller
+                    control={control}
+                    name={`devlinks[${index}].platform`}
+                    render={({ field: { onChange, value } }) => (
+                        <Select
+                            value={value}
+                            onValueChange={(a) => onChange(a)}
+                        >
+                            <SelectTrigger className="h-[4.8rem] rounded-[.8rem] border border-borders pl-[1rem] pr-[1.5rem] text-[1.6rem] text-grey aria-expanded:border-primary aria-expanded:shadow-drop_primary">
+                                <SelectValue />
+                            </SelectTrigger>
+                            <SelectContent className="z-50 bg-white px-[1.6rem]">
+                                {Object.values(Platforms).map((platform) => (
+                                    <SelectItem
+                                        key={platform.id}
+                                        value={platform.icon}
+                                        className="border-b-borders px-0 py-[1.2rem] text-[1.6rem] text-dark_grey [&:not(:last-of-type)]:border-b"
+                                    >
+                                        <div className="flex items-center gap-[1rem]">
+                                            <PlatformIcon
+                                                platform={platform.icon}
+                                            />
+                                            {platform.label}
+                                        </div>
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    )}
+                />
             </div>
 
             <div>
@@ -74,6 +87,7 @@ const DevLinkItem = ({ link, index }: Props) => {
                 >
                     <LinkIcon className="shrink-0" />
                     <input
+                        {...register(`devlinks[${index}].link`)}
                         className="input:-internal-autofill-selected: w-full bg-transparent text-dark_grey outline-none placeholder:text-dark_grey placeholder:opacity-50"
                         type="text"
                         placeholder="e.g https://github.com/jownsu"
--- app/(pages)/links/_components/LinkList.tsx
+++ app/(pages)/links/_components/LinkList.tsx
@@ -1,53 +1,97 @@
 "use client";
 
 import Button from "@/app/_components/Button";
-import useDevLinkStore from "@/app/_store/devlink.store";
+import { DevLink } from "@/app/_constants/constants";
 import EmptyIcon from "@/public/icons/illustration-empty.svg";
+import { FormProvider, useFieldArray, useForm } from "react-hook-form";
 import DevLinkItem from "./DevLinkItem";
 
+type FormValues = {
+    devlinks: DevLink[];
+};
+
 const LinkList = () => {
-    const devlinks = useDevLinkStore((state) => state.devlinks);
-    const addDevLink = useDevLinkStore((state) => state.addDevLink);
+    const methods = useForm<FormValues>({
+        defaultValues: {
+            devlinks: []
+        }
+    });
+
+    const {
+        fields: devlinks,
+        append,
+        remove
+    } = useFieldArray({
+        control: methods.control,
+        name: "devlinks"
+    });
+
+    const onSubmit = (data: FormValues) => {
+        console.log(data);
+    };
 
     return (
-        <div className="flex flex-1 flex-col overflow-hidden">
-            <Button
-                variant={"outline"}
-                size={"full"}
-                type="submit"
-                className="mb-[2.4rem]"
-                onClick={addDevLink}
+        <FormProvider {...methods}>
+            <form
+                className="flex flex-1 flex-col overflow-hidden"
+                onSubmit={methods.handleSubmit(onSubmit)}
             >
-                + Add new link
-            </Button>
-            {!!devlinks.length && (
-                <ul className="mb-[2.4rem] flex flex-1 flex-col gap-[2.4rem] overflow-y-auto">
-                    {devlinks.map((link, index) => (
-                        <DevLinkItem key={link.id} link={link} index={index} />
-                    ))}
-                </ul>
-            )}
+                <Button
+                    variant={"outline"}
+                    size={"full"}
+                    type="button"
+                    className="mb-[2.4rem]"
+                    onClick={() =>
+                        append({
+                            id: Math.floor(Math.random() * 9999999),
+                            platform: "github",
+                            link: ""
+                        })
+                    }
+                >
+                    + Add new link
+                </Button>
 
-            {!devlinks.length && (
-                <div className="mb-[2.4rem] flex flex-1 flex-col justify-center rounded-[1.2rem] bg-light_grey px-[2rem] py-[4.6rem] text-center">
-                    <EmptyIcon className="mx-auto mb-[2.4rem] h-[8rem] w-[12.477rem]" />
-                    <h2 className="mb-[2.2rem] text-[2.4rem] font-bold text-dark_grey md:text-[3.2rem]">
-                        Let&apos;s get you started
-                    </h2>
-                    <p className="mx-auto text-dark_grey md:max-w-[49rem]">
-                        Use the &quot;Add new link&quot; button to get started.
-                        Once you have more than one link, you can reorder and
-                        edit them. We&apos;re here to help you share your
-                        profiles with everyone!
-                    </p>
-                </div>
-            )}
+                {!!devlinks.length && (
+                    <ul className="mb-[2.4rem] flex flex-1 flex-col gap-[2.4rem] overflow-y-auto">
+                        {devlinks.map((link, index) => (
+                            <DevLinkItem
+                                key={link.id}
+                                link={link}
+                                index={index}
+                                remove={remove}
+                            />
+                        ))}
+                    </ul>
+                )}
 
-            <div className="mb-[2.4rem] h-[.1rem] w-[calc(100%_+_4.8rem)] translate-x-[-2.4rem] bg-borders"></div>
-            <Button size="full" disabled className="md:ml-auto md:w-fit">
-                Save
-            </Button>
-        </div>
+                {!devlinks.length && (
+                    <div className="mb-[2.4rem] flex flex-1 flex-col justify-center rounded-[1.2rem] bg-light_grey px-[2rem] py-[4.6rem] text-center">
+                        <EmptyIcon className="mx-auto mb-[2.4rem] h-[8rem] w-[12.477rem]" />
+                        <h2 className="mb-[2.2rem] text-[2.4rem] font-bold text-dark_grey md:text-[3.2rem]">
+                            Let&apos;s get you started
+                        </h2>
+                        <p className="mx-auto text-dark_grey md:max-w-[49rem]">
+                            Use the &quot;Add new link&quot; button to get
+                            started. Once you have more than one link, you can
+                            reorder and edit them. We&apos;re here to help you
+                            share your profiles with everyone!
+                        </p>
+                    </div>
+                )}
+
+                <div className="mb-[2.4rem] h-[.1rem] w-[calc(100%_+_4.8rem)] translate-x-[-2.4rem] bg-borders"></div>
+
+                <Button
+                    size="full"
+                    disabled={!devlinks.length}
+                    className="md:ml-auto md:w-fit"
+                    type="submit"
+                >
+                    Save
+                </Button>
+            </form>
+        </FormProvider>
     );
 };
 
