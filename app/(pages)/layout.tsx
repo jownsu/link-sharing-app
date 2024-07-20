6001ca401f55d53feb33a2e5bc45da7a3c502068
Jownsu <jhonesdigno666@gmail.com>
2024-07-20 21:09:43 +0800

    Move the phone illustration to layout

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,5 +1,7 @@
 import NavBar from "../_components/NavBar";
 import DevlinkFormProvider from "../_providers/devlink_form_provider";
+import PhoneIllustration from "@/public/icons/illustration-phone-mockup.svg";
+import PreviewList from "./links/_components/PreviewList";
 import "../globals.css";
 
 export default function RootLayout({
@@ -10,7 +12,19 @@ export default function RootLayout({
     return (
         <main className="flex max-h-screen min-h-screen flex-col bg-light_grey">
             <NavBar />
-            <DevlinkFormProvider>{children}</DevlinkFormProvider>
+            <DevlinkFormProvider>
+                <div className="container flex min-h-full flex-1 gap-[2.4rem] py-[2.4rem]">
+                    <div className="hidden w-[56rem] place-items-center rounded-[1.2rem] bg-white lg:grid">
+                        <div className="relative h-[63.1rem] w-[30.7rem]">
+                            <PhoneIllustration className="h-full w-full" />
+                            <PreviewList />
+                        </div>
+                    </div>
+                    <div className="flex-1 rounded-[1.2rem] bg-white p-[2.4rem]">
+                        {children}
+                    </div>
+                </div>
+            </DevlinkFormProvider>
         </main>
     );
 }
--- app/(pages)/links/page.tsx
+++ app/(pages)/links/page.tsx
@@ -1,26 +1,16 @@
 import LinkList from "./_components/LinkList";
-import PhoneIllustration from "@/public/icons/illustration-phone-mockup.svg";
-import PreviewList from "./_components/PreviewList";
 
 const LinkPage = () => {
     return (
-        <div className="container flex min-h-full flex-1 gap-[2.4rem] py-[2.4rem]">
-            <div className="hidden w-[56rem] place-items-center rounded-[1.2rem] bg-white lg:grid">
-                <div className="relative h-[63.1rem] w-[30.7rem]">
-                    <PhoneIllustration className="h-full w-full" />
-                    <PreviewList />
-                </div>
-            </div>
-            <div className="flex flex-1 flex-col rounded-[1.2rem] bg-white p-[2.4rem]">
-                <h1 className="text-[2.4rem] font-bold leading-[150%] text-dark_grey md:text-[3.2rem]">
-                    Customize your links
-                </h1>
-                <p className="mb-[4.1rem] text-grey">
-                    Add/edit/remove links below and then share all your profiles
-                    with the world!
-                </p>
-                <LinkList />
-            </div>
+        <div className="flex h-full flex-1 flex-col">
+            <h1 className="text-[2.4rem] font-bold leading-[150%] text-dark_grey md:text-[3.2rem]">
+                Customize your links
+            </h1>
+            <p className="mb-[4.1rem] text-grey">
+                Add/edit/remove links below and then share all your profiles
+                with the world!
+            </p>
+            <LinkList />
         </div>
     );
 };
