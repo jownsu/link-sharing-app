d532ab650f3fdf2805b0c328da45a380a6f963e8
Jownsu <jhonesdigno666@gmail.com>
2024-07-26 11:58:08 +0800

    RWD of profile details page

--- app/(pages)/profile/page.tsx
+++ app/(pages)/profile/page.tsx
@@ -11,11 +11,13 @@ const ProfilePage = () => {
                 Add your details to create a personal touch to your profile.
             </p>
 
-            <div className="mb-[2.4rem] rounded-[1.2rem] bg-light_grey p-[2rem]">
-                <p className="mb-[1.6rem] text-grey">Profile picture</p>
+            <div className="mb-[2.4rem] flex flex-col rounded-[1.2rem] bg-light_grey p-[2rem] md:flex-row md:items-center md:gap-[1.2rem] lg:flex-col lg:items-start lg:gap-[0] xl:flex-row xl:items-center xl:gap-[1.2rem]">
+                <p className="mb-[1.6rem] text-grey md:shrink-0 md:basis-[24rem] lg:basis-0 xl:basis-[24rem]">
+                    Profile picture
+                </p>
                 <label
                     htmlFor="profile_picture"
-                    className="mb-[2.4rem] flex size-[19.3rem] flex-col items-center justify-center rounded-[1.2rem] bg-primary_lighter"
+                    className="mb-[2.4rem] flex size-[19.3rem] shrink-0 flex-col items-center justify-center rounded-[1.2rem] bg-primary_lighter md:mr-[1.1rem]"
                 >
                     <ImageIcon className="size-[4rem]" />
                     <span className="text-[1.6rem] font-semibold text-primary">
@@ -29,16 +31,16 @@ const ProfilePage = () => {
                     id="profile_picture"
                     accept="image/png, image/jpeg"
                 />
-                <p className="text-[1.2rem] text-grey">
+                <p className="text-[1.2rem] text-grey md:max-w-[13rem] lg:w-auto xl:max-w-[13rem]">
                     Image must be below 1024x1024px. Use PNG or JPG format.
                 </p>
             </div>
 
-            <div className="flex flex-col gap-[.6rem] rounded-[1.2rem] bg-light_grey p-[2rem]">
-                <div>
+            <div className="flex flex-col gap-[.6rem] rounded-[1.2rem] bg-light_grey p-[2rem] md:gap-[1.2rem]">
+                <div className="flex flex-col md:flex-row md:items-center md:gap-[1.6rem] lg:flex-col lg:items-start lg:gap-0 xl:flex-row xl:items-center xl:gap-[1.6rem]">
                     <label
                         htmlFor="first_name"
-                        className="mb-[.5rem] inline-block text-[1.2rem] text-dark_grey"
+                        className="mb-[.5rem] inline-block text-[1.2rem] text-dark_grey md:shrink-0 md:basis-[24rem] md:text-[1.6rem] md:text-grey lg:basis-0 xl:basis-[24rem]"
                     >
                         First name*
                     </label>
@@ -51,10 +53,10 @@ const ProfilePage = () => {
                         />
                     </div>
                 </div>
-                <div>
+                <div className="flex flex-col md:flex-row md:items-center md:gap-[1.6rem] lg:flex-col lg:items-start lg:gap-0 xl:flex-row xl:items-center xl:gap-[1.6rem]">
                     <label
                         htmlFor="first_name"
-                        className="mb-[.5rem] inline-block text-[1.2rem] text-dark_grey"
+                        className="mb-[.5rem] inline-block text-[1.2rem] text-dark_grey md:shrink-0 md:basis-[24rem] md:text-[1.6rem] md:text-grey lg:basis-0 xl:basis-[24rem]"
                     >
                         Last name*
                     </label>
@@ -67,10 +69,10 @@ const ProfilePage = () => {
                         />
                     </div>
                 </div>
-                <div>
+                <div className="flex flex-col md:flex-row md:items-center md:gap-[1.6rem] lg:flex-col lg:items-start lg:gap-0 xl:flex-row xl:items-center xl:gap-[1.6rem]">
                     <label
                         htmlFor="first_name"
-                        className="mb-[.5rem] inline-block text-[1.2rem] text-dark_grey"
+                        className="mb-[.5rem] inline-block text-[1.2rem] text-dark_grey md:shrink-0 md:basis-[24rem] md:text-[1.6rem] md:text-grey lg:basis-0 xl:basis-[24rem]"
                     >
                         Email
                     </label>
