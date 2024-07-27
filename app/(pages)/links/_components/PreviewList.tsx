fa46006bd33b244750cf1340f670da721b4fccb5
Jownsu <jhonesdigno666@gmail.com>
2024-07-27 13:47:03 +0800

    Placeholder for devlinks

--- app/(pages)/links/_components/PreviewList.tsx
+++ app/(pages)/links/_components/PreviewList.tsx
@@ -5,17 +5,26 @@ import { DevlinkForm } from "@/app/_constants/constants";
 import { useFormContext } from "react-hook-form";
 
 const PreviewList = () => {
-
     const { watch } = useFormContext<DevlinkForm>();
     const devlinks = watch("devlinks");
 
     return (
         <div className="absolute bottom-[5.3rem] left-[50%] flex h-[30rem] max-h-[30rem] w-[23.7rem] -translate-x-1/2 flex-col gap-[2rem] overflow-auto">
-            {
+            {!!devlinks.length &&
                 devlinks.map((devlink) => (
-                    <PreviewLink key={devlink.id} href={devlink.link} platform={devlink.platform} />
-                ))
-            }
+                    <PreviewLink
+                        key={devlink.id}
+                        href={devlink.link}
+                        platform={devlink.platform}
+                    />
+                ))}
+            {!devlinks.length &&
+                Array.from({ length: 5 }).map((_, index) => (
+                    <div
+                        key={index}
+                        className="h-[4.4rem] w-full rounded-[.8rem] bg-[#eee]"
+                    ></div>
+                ))}
         </div>
     );
 };
