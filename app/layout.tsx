0c9f08c071b627184677498d032da34157e2b1a5
Jownsu <jhonesdigno666@gmail.com>
2024-08-17 12:16:42 +0800

    Implement authorization through useEffect and localstorage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,27 @@
 import type { Metadata } from "next";
 import { Instrument_Sans } from "next/font/google";
-import QueryClientProvider from "./_utils/QueryClientProvider";
+import AuthProvider from "./_utils/AuthProvider";
 import "./globals.css";
 
 const instrument_sans = Instrument_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Link Sharing App",
-	description: "Link sharing app that can customize and share your profile links"
+    title: "Link Sharing App",
+    description:
+        "Link sharing app that can customize and share your profile links"
 };
 
 export default function RootLayout({
-	children
+    children
 }: Readonly<{
-	children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<link rel="icon" href="/favicon-32x32.png" sizes="any" />
-			<body className={instrument_sans.className}>
-				<QueryClientProvider>{children}</QueryClientProvider>
-			</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <link rel="icon" href="/favicon-32x32.png" sizes="any" />
+            <body className={instrument_sans.className}>
+                <AuthProvider>{children}</AuthProvider>
+            </body>
+        </html>
+    );
 }
