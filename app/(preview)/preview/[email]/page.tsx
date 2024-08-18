2537756fbc87f6270d1cf4db5e80e9c2cae85cb9
Jownsu <jhonesdigno666@gmail.com>
2024-08-18 12:34:29 +0800

    Implement desktop view

--- app/(preview)/preview/[email]/page.tsx
+++ app/(preview)/preview/[email]/page.tsx
@@ -31,8 +31,8 @@ const PreviewPage = ({ params }: Props) => {
     return (
         <div>
             <div className="absolute top-0 z-[-10] hidden h-[35.7rem] w-full rounded-b-[3.2rem] bg-primary md:block"></div>
-            <div className="md:p-[2.4rem]">
-                <div className="mb-[6rem] flex gap-[1.6rem] rounded-[1.2rem] px-[2.4rem] py-[1.6rem] md:justify-between md:bg-white">
+            <div className="mb-[6rem] md:mb-[10.2rem] md:p-[2.4rem] lg:mb-[8.1rem]">
+                <div className="flex gap-[1.6rem] rounded-[1.2rem] px-[2.4rem] py-[1.6rem] md:justify-between md:bg-white">
                     <Button
                         variant={"outline"}
                         className="flex-1 px-0 md:flex-none md:px-[2.7rem]"
@@ -47,7 +47,7 @@ const PreviewPage = ({ params }: Props) => {
 
             <div className="md:shadow-drop_grey mx-auto max-w-[23.7rem] bg-white text-center md:max-w-[34.9rem] md:rounded-[2.4rem] md:px-[5.6rem] md:py-[4.8rem]">
                 <div className="mb-[5.6rem]">
-                    <div className="mx-auto mb-[2.5rem] size-[9.6rem] overflow-hidden rounded-full outline outline-[.4rem] outline-primary">
+                    <div className="mx-auto mb-[2.5rem] size-[10.4rem] overflow-hidden rounded-full outline outline-[.4rem] outline-primary">
                         <Image
                             className="h-full w-full object-cover"
                             src={data.img!}
